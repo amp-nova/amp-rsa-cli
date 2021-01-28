81de126016870701a99e871bd5ab07674f425813
Reza Kalfane <reza@kalfane.org>
2021-01-28 14:03:46 +0100

    CLI base files

---
+++ src/cli.ts
@@ -0,0 +1,35 @@
+import Yargs from 'yargs/yargs';
+import YargsCommandBuilderOptions from './common/yargs/yargs-command-builder-options';
+import { Arguments, Argv } from 'yargs';
+
+const configureYargs = (yargInstance: Argv): Promise<Arguments> => {
+  return new Promise(
+    async (resolve): Promise<void> => {
+      let failInvoked = false;
+      const isYError = (err?: Error | string): boolean => err instanceof Error && err.name === 'YError';
+      const failFn = (msg: string, err?: Error | string): void => {
+        // fail should only be invoked once
+        if (failInvoked) {
+          return;
+        }
+        failInvoked = true;
+        if ((msg && !err) || isYError(err)) {
+          yargInstance.showHelp('error');
+        }
+      };
+      const argv = await yargInstance
+        .scriptName('dc-cli')
+        .commandDir('./commands', YargsCommandBuilderOptions)
+        .strict()
+        .demandCommand(1, 'Please specify at least one command')
+        .exitProcess(false)
+        .showHelpOnFail(false)
+        .fail(failFn).argv;
+      resolve(argv);
+    }
+  );
+};
+
+export default async (yargInstance = Yargs(process.argv.slice(2))): Promise<Arguments | void> => {
+  return await configureYargs(yargInstance);
+};

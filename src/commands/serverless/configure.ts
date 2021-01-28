2e3a86994fc9d334f016ddabeab89b6866197072
Reza Kalfane <reza@kalfane.org>
2021-01-29 00:17:14 +0100

    Configure serverless services

--- src/commands/serverless/configure.ts
+++ src/commands/serverless/configure.ts
@@ -1,4 +1,8 @@
 import { Arguments } from 'yargs';
+import { readFileSync, writeFileSync } from 'fs';
+import { compile as handlebarsCompile } from 'handlebars';
+
+const yaml = require('js-yaml');
 
 export const command = 'configure';
 export const desc = "Configure AWS serverless stacks";
@@ -6,7 +10,34 @@ export const desc = "Configure AWS serverless stacks";
 export const handler = async (
   argv: Arguments
 ): Promise<void> => {
-  console.log("Configuring willow-content-delivery-poc");
-  console.log("Configuring willow-demo-services");
 
+  const serverless = [
+    "dc-content-delivery-poc",
+    "willow-demo-services"
+  ];
+
+  // Reading settings
+  const settings = readFileSync(`./settings.yaml`).toString();
+
+  // Converting from YAML to JSON
+  const settingsJson = yaml.load(settings)
+  console.log('Settings loaded');
+
+  try {
+    serverless.map((item: string) => {
+
+      // Getting and compiling serverless config tempalte
+      const configTemplate = readFileSync(`./assets/serverless/${item}/serverless.yml.hbs`).toString();
+      const configTemplateCompiled = handlebarsCompile(configTemplate);
+
+      // Applying settings to template
+      const serverlessConfigYAML = configTemplateCompiled(settingsJson);
+
+      // Write serverless config to file
+      writeFileSync(`./repositories/${item}/serverless.yml`, serverlessConfigYAML);
+      console.log(`Wrote serverless config to file ./repositories/${item}/serverless.yml`)
+    });
+  } catch(error) {
+    console.log(error.message);
+  }
 };

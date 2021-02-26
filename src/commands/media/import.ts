ae77a888524128ce6e4ab58a91fa4c319c6a5c2f
Reza Kalfane <reza.kalfane@gmail.com>
2021-02-26 19:21:38 +0100

    Importing assets from S3 bucket

--- src/commands/media/import.ts
+++ src/commands/media/import.ts
@@ -1,4 +1,9 @@
 import { Arguments } from 'yargs';
+import { readFileSync, writeFileSync } from 'fs';
+import childProcess from 'child_process';
+
+const yaml = require('js-yaml');
+const lodash = require('lodash');
 
 export const command = 'import';
 export const desc = "Import media assets";
@@ -6,7 +11,28 @@ export const desc = "Import media assets";
 export const handler = async (
   argv: Arguments
 ): Promise<void> => {
-  console.log("Importing content media");
-  console.log("Importing product media");
+  try {
 
+    // Reading global settings
+    let settingsYAML = readFileSync(`./settings.yaml`).toString();
+
+    // Backup settings
+    writeFileSync("settings.yaml.backup", settingsYAML);
+
+    // Converting from YAML to JSON
+    const settingsJSON = yaml.load(settingsYAML)
+    console.log('Global settings loaded');
+
+    const sourceType = settingsJSON.dam.source.type;
+    if (sourceType === 's3') {
+
+      const bucket = settingsJSON.dam.source.bucket;
+      const region = settingsJSON.dam.source.region;
+      childProcess.execSync(
+        `dam-cli assets import-s3 ${bucket} ${region}`
+      );
+    }
+  } catch(error) {
+    console.log(error.message);
+  }
 };

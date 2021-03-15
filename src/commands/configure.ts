32884bf1bea39e2a9788d0402827d3ce48816151
Reza Kalfane <reza.kalfane@gmail.com>
2021-03-15 11:14:40 +0100

    Updated configure

--- src/commands/configure.ts
+++ src/commands/configure.ts
@@ -25,15 +25,21 @@ export const handler = async (
         `dc-cli configure --clientId ${settingsJSON.cms.clientId} --clientSecret ${settingsJSON.cms.clientSecret} --hubId ${settingsJSON.cms.hubId}`
       );
 
-    console.log('Configure dam-cli...');
-    childProcess.execSync(
-      `dam-cli configure --username ${settingsJSON.dam.username} --password ${settingsJSON.dam.password}`
-    )
+    // Configure DAM CLI if needed
+    if (settingsJSON.dam.username) {
+      console.log('Configure dam-cli...');
+      childProcess.execSync(
+        `dam-cli configure --username ${settingsJSON.dam.username} --password ${settingsJSON.dam.password}`
+      )
+    }
 
+    // Configure AWS CLI if needed
+    if (settingsJSON.serverless.accessKeyId) {
     console.log('Configure aws cli...');
-    childProcess.execSync(
-      `serverless config credentials --provider aws --key ${settingsJSON.serverless.accessKeyId} --secret ${settingsJSON.serverless.secretAccessKey} --profile ${settingsJSON.serverless.customProfileName}`
-    )
+      childProcess.execSync(
+        `serverless config credentials --provider aws --key ${settingsJSON.serverless.accessKeyId} --secret ${settingsJSON.serverless.secretAccessKey} --profile ${settingsJSON.serverless.customProfileName}`
+      )
+    }
   } catch(error) {
     console.log(error.message);
   }

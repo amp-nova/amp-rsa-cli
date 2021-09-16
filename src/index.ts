27585145414a96305a62d25b0ed475fc518ea3e0
Dave Lilly <dave@lillys.org>
2021-09-16 13:13:53 -0500

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,43 @@
 #!/usr/bin/env node
 
-import cli from './cli';
+// const cli = require('./cli')
+// cli();
 
-cli();
+import Yargs from 'yargs/yargs';
+import YargsCommandBuilderOptions from './common/yargs/yargs-command-builder-options';
+import { Arguments, Argv } from 'yargs';
+
+const configureYargs = (yargInstance: Argv): Promise<Arguments> => {
+  return new Promise(
+    async (resolve): Promise<void> => {
+      let failInvoked = false;
+      const isYError = (err?: Error | string): boolean => err instanceof Error && err.name === 'YError';
+      const failFn = (msg: string, err?: Error | string): void => {
+        // fail should only be invoked once
+        if (failInvoked) {
+          return;
+        }
+        failInvoked = true;
+        if ((msg && !err) || isYError(err)) {
+          yargInstance.showHelp('error');
+        }
+      };
+      const argv = yargInstance
+        .scriptName('demo-cli')
+        .usage('Usage: $0 <command> [options]')
+        .commandDir('./commands', YargsCommandBuilderOptions)
+        .strict()
+        .demandCommand(1, 'Please specify at least one command')
+        .exitProcess(false) 
+        .showHelpOnFail(false)
+        .fail(failFn).argv;
+      resolve(argv);
+    }
+  );
+};
+
+const yargInstance = Yargs(process.argv.slice(2))
+configureYargs(yargInstance)
+export default async (yargInstance = Yargs(process.argv.slice(2))): Promise<Arguments | void> => {
+  return await configureYargs(yargInstance);
+};

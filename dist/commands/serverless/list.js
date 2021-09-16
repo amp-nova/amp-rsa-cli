fbee16c20590c3f7b528f18e0c84075e28561c89
Dave Lilly <dave@lillys.org>
2021-09-16 12:25:37 -0500

    $1

--- dist/commands/serverless/list.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handler = exports.desc = exports.command = void 0;
-exports.command = 'list';
-exports.desc = "List AWS serverless services";
-const handler = async (argv) => {
-    console.log("Listing serverless services");
-};
-exports.handler = handler;
--- dist/common/yargs/yargs-command-builder-options.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const YargsCommandBuilderOptions = { extensions: ['ts', 'js'], exclude: /\.spec\.ts$/ };
-exports.default = YargsCommandBuilderOptions;

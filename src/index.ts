6187f4e3ed01bcb0b2a72464995cc8231082a5aa
Dave Lilly <dave@lillys.org>
2021-09-16 13:23:38 -0500

    merge

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,4 @@
 #!/usr/bin/env node
 
-// const cli = require('./cli')
-// cli();
-
-// import Yargs from 'yargs/yargs';
-// import YargsCommandBuilderOptions from './common/yargs/yargs-command-builder-options';
-// import { Arguments, Argv } from 'yargs';
-
-const Yargs = require('yargs/yargs')
-const YargsCommandBuilderOptions = require('./common/yargs/yargs-command-builder-options');
-// const { Arguments, Argv } = require('@types/yargs');
-
-const configureYargs = (yargInstance: any): Promise<any> => {
-  return new Promise(
-    async (resolve): Promise<void> => {
-      let failInvoked = false;
-      const isYError = (err?: Error | string): boolean => err instanceof Error && err.name === 'YError';
-      const failFn = (msg: string, err?: Error | string): void => {
-        // fail should only be invoked once
-        if (failInvoked) {
-          return;
-        }
-        failInvoked = true;
-        if ((msg && !err) || isYError(err)) {
-          yargInstance.showHelp('error');
-        }
-      };
-      const argv = yargInstance
-        .scriptName('demo-cli')
-        .usage('Usage: $0 <command> [options]')
-        .commandDir('./commands', YargsCommandBuilderOptions)
-        .strict()
-        .demandCommand(1, 'Please specify at least one command')
-        .exitProcess(false) 
-        .showHelpOnFail(false)
-        .fail(failFn).argv;
-      resolve(argv);
-    }
-  );
-};
-
-const yargInstance = Yargs(process.argv.slice(2))
-configureYargs(yargInstance)
-export default async (yargInstance = Yargs(process.argv.slice(2))): Promise<any | void> => {
-  return await configureYargs(yargInstance);
-};
+import cli from './cli';
+cli();

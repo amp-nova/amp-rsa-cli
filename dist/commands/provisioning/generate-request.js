0006b9b6a26ec57b780cb4bbe0dae582510f4b1a
Dave Lilly <dave@lillys.org>
2021-09-20 12:13:51 -0500

    .

--- dist/commands/provisioning/generate-request.js
+++ dist/commands/provisioning/generate-request.js
@@ -1,11 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.handler = exports.desc = exports.command = void 0;
+exports.handler = exports.builder = exports.desc = exports.command = void 0;
 const handlebars_1 = require("handlebars");
+const settings_handler_1 = require("../../common/settings-handler");
 const fs = require('fs-extra');
 const yaml = require('js-yaml');
 exports.command = 'generate-request';
 exports.desc = "Generate Provisioning Request";
+exports.builder = settings_handler_1.settingsBuilder;
 const handler = async (argv) => {
     try {
         let provisioningYAML = fs.readFileSync(`${argv.automationDir}/provisioning.yaml`).toString();
